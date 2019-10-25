9dc581c441330a4050b9b918f9ef8e3d1978e29a
Wendy Harris <wendyv510@hotmail.com>
2019-10-25 21:38:52 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,73 @@
-# Make your shoe class here!
+class Shoe 
+  
+  def initialize(brand)
+    @brand = brand 
+  end 
+  
+  def brand
+    @brand
+  end 
+  
+  def properties(brand)
+    @brand = brand
+  end 
+  
+  def brand 
+    @brand
+  end 
+  
+  def color(color)
+    @color = color
+  end 
+  
+  def color
+    @color
+  end 
+  
+  def shoe_size(shoe_size)
+    @shoe_size = shoe_size
+  end 
+  
+  def shoe_size
+    @shoe_size
+  end 
+  
+  def material(material)
+    @material = material
+  end 
+  
+  def material
+    @material
+  end 
+  
+  def shoe_condition(condition)
+    @condition = condition
+  end 
+  
+  def condition
+    @condition
+  end 
+  
+  def cobble 
+    puts "Your shoe is as good as new"
+    shoe_condition.each do |condition|
+      {|condition.new}
+    end 
+  end 
+end  
+
+Shoe.new("Nike")
+
+Shoe.new("Adidas")
+
+Shoe.brand("Nike")
+
+Shoe.color("Red")
+
+Shoe.shoe_size(9.5)
+
+Shoe.material("suede")
+
+Shoe.shoe_condition("tattered")
+
+Shoe.cobble 
