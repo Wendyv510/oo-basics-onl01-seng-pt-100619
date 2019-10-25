b0adfe86281dfcb517e32cd4fcda310ba119abc8
Wendy Harris <wendyv510@hotmail.com>
2019-10-25 21:59:53 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -15,41 +15,10 @@ class Shoe
   def brand 
     @brand
   end 
-end 
+
   
-  class Properties 
+  attr_accessor(shoe_properties) :color, :size, :material, :condition 
   
-  def color(color)
-    @color = color
-  end 
-  
-  def color
-    @color
-  end 
-  
-  def shoe_size(shoe_size)
-    @shoe_size = shoe_size
-  end 
-  
-  def shoe_size
-    @shoe_size
-  end 
-  
-  def material(material)
-    @material = material
-  end 
-  
-  def material
-    @material
-  end 
-  
-  def shoe_condition(condition)
-    @condition = condition
-  end 
-  
-  def condition
-    @condition
-  end 
   
   def cobble 
     puts "Your shoe is as good as new"
